e667f1d2a39f43ca0d0d8ac07c7d4bca05b836ae
Don_Aveo <133364365+BrendonAvelino@users.noreply.github.com>
2024-06-07 10:38:12 -0300

    adicionandoArquivos

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { bancoExterno } from './firebaseConnection';
+import { useEffect, useState } from 'react';
+import { doc, onSnapshot } from 'firebase/firestore';
+
 
 export default function App() {
+
+  const [nome, setNome] = useState('Carregando...');
+
+  useEffect(() => {
+    onSnapshot(doc(bancoExterno, "/", "/"), (snap) => {
+      setNome(snap.data()?.Geladeira)
+    });
+
+
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ fontSize: 25 }}>Informação: {nome}</Text>
+      <TouchableOpacity style={{ backgroundColor: "#F94", marginTop: 20, borderRadius: 5, padding: 10 }}>
+        <Text style={{ fontSize: 20, paddingHorizontal: 15 }}>Adicionar</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
